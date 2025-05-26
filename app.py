c4b7aecb4d4f7690e4aa93370a7d12a5c6516577
agw2005 <danial.agw2005@gmail.com>
2025-05-26 10:05:22 +0700

    fixed import error, again

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ yang berisi library python yang dipakai agar aplikasi bisa berjalan.
 import streamlit as st
 # # import cv2
 # import numpy as np
-# from PIL import Image
+from PIL import Image
 from image_utils import *
 
 st.set_page_config("Aplikasi Pengolahan Citra", layout="wide")
